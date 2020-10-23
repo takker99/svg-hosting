97624ac13af32d16abf55a0c9a2d2cef2e309c68
takker99 <37929109+takker99@users.noreply.github.com>
2020-10-23 16:49:03 +0900

    :+1: 修正

--- api/main.ts
+++ api/main.ts
@@ -1,25 +1,24 @@
-import { NowRequest, NowResponse } from '@vercel/node';
+import {NowRequest, NowResponse} from '@vercel/node';
 
 export default async (request: NowRequest, response: NowResponse) => {
-  const { svg } = request.query;
-  let svgUrl = '';
-  try {
-    svgUrl = new URL(svg);
-  } catch (error) {
-    response.status(400).end(error.message);
-  }
+    const {svg} = request.query;
 
-  let resSvgText = '';
-  try {
-    resSvgText = await fetch(svgUrl).then(res => res.text);
-  } catch (error) {
-    response.status(400).end(error.message);
-  }
+    let resSvgText = '';
+    try {
+        const res = await fetch(svg);
+        if (!res.ok) throw new Error('No image response');
+        const contentType = res.headers.get('content-type') ?? '';
+        if (!['text/plain', 'text/xml', 'image/svg+xml'].includes(contentType))
+            throw Error(`Invalid content-type: ${contentType}`);
+        resSvgText = await res.text();
+    } catch (error) {
+        response.status(400).end(error.message);
+    }
 
-  response.writeHead(200, {
-    'Content-Type': 'image/svg+xml',
-    'Cache-Control': `private, max-age=${60 * 60 * 24}` // 1 day
-  });
-  response.end(resSvgText);
+    response.writeHead(200, {
+        'Content-Type': 'image/svg+xml',
+        'Cache-Control': `private, max-age=${60 * 60 * 24}` // 1 day
+    });
+    response.end(resSvgText);
 };
 
